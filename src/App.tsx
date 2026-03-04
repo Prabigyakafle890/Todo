ecdd709c0cb71cb20349b8a9e579765f208f96e1
Prabigya Kafle <prabigyakafle50@gmail.com>
2026-03-04 14:06:49 +0545

    feat: implement local storage functionality for todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import type { todoItem } from "./types";
-import List from "./components/List";
+
+
+function getRand(){
+    return new Date().getTime().toString() + Math.floor(Math.random()*1000000);
+}
 function App() {
   const [todos, setTodos] = useState<todoItem[]>([]);
+  
+ 
 
   const addTodo = (title: string) => {
-    setTodos([...todos, { id: crypto.randomUUID(), title }]);
+    setTodos([...todos, { id: getRand(), title }]);
   };
 
+ 
   return (
     <>
       <Header />
       <Form addTodo={addTodo} />
-      <List todos={todos} />
+     
     </>
   );
 }
