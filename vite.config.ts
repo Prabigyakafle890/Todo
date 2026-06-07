470d12b1631bfe10df424d04a8ee12e05b5d01c3
Prabigyakafle890 <155903898+Prabigyakafle890@users.noreply.github.com>
2026-06-07 13:00:59 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const allowedHosts = env.VITE_ALLOWED_HOSTS
+    ? env.VITE_ALLOWED_HOSTS.split(",")
+    : [];
+
+  return {
+    plugins: [tailwindcss()],
+    preview: {
+      allowedHosts,
+    },
+  };
 });
